43a681ed3e6ab5d45125fe1fd437c7d3025ea77a
Laparevich Daniil <danik.laparevich@bk.ru>
2023-07-03 23:31:19 +0300

    create: design for application
    add: onAction events to add numbers and math operators
    
    add: clear and backspace functionality for buttons

--- src/main/java/com/example/javacalculator/HelloController.java
+++ src/main/java/com/example/javacalculator/HelloController.java
@@ -1,14 +1,71 @@
 package com.example.javacalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private StringBuilder outPutExpression;
+
+    public TextField expression = new TextField("0");
+
+    private String mathOperator;
+
+    public void insertNumber(ActionEvent event) {
+        Button btn = (Button) event.getSource();
+        outPutExpression = new StringBuilder(expression.getText());
+        outPutExpression.append(btn.getText());
+        expression.setText(outPutExpression.toString());
+    }
+
+    public void insertMathOperator(ActionEvent event) {
+        if (expression.getLength() > 0 && !(outPutExpression.substring(outPutExpression.length() - 1).equals("+")) && !(outPutExpression.substring(outPutExpression.length() - 1).equals("-")) && !(outPutExpression.substring(outPutExpression.length() - 1).equals("*")) && !(outPutExpression.substring(outPutExpression.length() - 1).equals("/"))) {
+            Button btn = (Button) event.getSource();
+            outPutExpression.append(btn.getText());
+            expression.setText(outPutExpression.toString());
+            mathOperator = btn.getText();
+        }
+    }
+
+    public void clear(ActionEvent event) {
+        if (expression.getLength() > 0) {
+            outPutExpression.delete(0, outPutExpression.length());
+            expression.setText(outPutExpression.toString());
+        }
+    }
+
+    public void backspace(ActionEvent event) {
+        if (expression.getLength() > 0) {
+            outPutExpression.delete(expression.getLength() - 1, expression.getLength());
+            expression.setText(outPutExpression.toString());
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void calculate() {
+        if (mathOperator.equals("+") || mathOperator.equals("*")) {
+            mathOperator = "\\\\" + mathOperator;
+        }
+        String[] tokens = expression.getText().split(mathOperator);
+        double expressionResult = 0;
+        switch (mathOperator) {
+            case "-" :
+                expressionResult = Double.parseDouble(tokens[0]) - Double.parseDouble(tokens[1]);
+                break;
+            case "/" :
+                expressionResult = Double.parseDouble(tokens[0]) / Double.parseDouble(tokens[1]);
+                break;
+            case "\\*" :
+                expressionResult = Double.parseDouble(tokens[0]) * Double.parseDouble(tokens[1]);
+                break;
+            case "\\+" :
+                expressionResult = Double.parseDouble(tokens[0]) + Double.parseDouble(tokens[1]);
+                break;
+        }
+
+        outPutExpression = new StringBuilder(String.valueOf(expressionResult));
+        expression.setText(outPutExpression.toString());
     }
 }
